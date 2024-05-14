669c4ce8a898d9589ea1059cbe6f7a76e260d329
Sachin007060 <sachinbaskandi20@gmail.com>
2024-05-14 22:52:37 +0530

    add Student1 class

---
+++ Student1.java
@@ -0,0 +1,18 @@
+package pack2;
+
+public class Student1{
+        private int rollNo;
+        private String name;
+        public void setRollNo(int r){
+            rollNo = r;
+        }
+        public void setName(String n){
+            name = n;
+        }
+        public int getRollNO(){
+            return rollNo;
+        }
+        public String getName(){
+            return name;
+        }
+}
